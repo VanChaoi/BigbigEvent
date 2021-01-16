dbb11455bc1b27d91adeb91e33fcdd1bd579aed3
VanChaoi <vanchaoi@163.com>
2021-01-16 12:19:56 +0800

    个人中心-重置密码-表单验证

--- user/js/repwd.js
+++ user/js/repwd.js
@@ -16,3 +16,20 @@ $("form").on("submit", function (e) {
     });
 });
 //表单验证-------------------------
+//1.密码长度6-12位(三个input都需要)
+//2.新密码和原密码不能一样(新密码使用)
+//3.两次新密码必需一致(重复密码使用)
+var form = layui.form;
+form.verify({
+    len: [/^\S{6,12}$/, "长度必需6~12位且不能出现空格"],
+    diff: function (val) {
+        if (val === $("input[name=oldPwd]").val()) {
+            return "新密码不能和原始密码相同";
+        }
+    },
+    same: function (val) {
+        if (val !== $("input[name=newPwd]").val()) {
+            return "两次密码不一致";
+        }
+    },
+});
